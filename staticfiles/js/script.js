7960ea718e74da59184276c853d9649a8b5057d2
your_name_here <msnsnavin@gmail.com>
2020-09-18 22:50:19 +0530

    rock paper scissor back end scripts completed

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -21,3 +21,47 @@ function catgenerator() {
 
 
 }
+
+//Challenge : Rock,paper,scissors
+function rpsGame(yourChoice) {
+    //console.log(yourChoice)
+    var humanchoice, botchoice;
+    humanchoice = yourChoice.id
+    botchoice = numberToChoice(randToRpsInt())
+    console.log(botchoice)
+    results = decideWinner(humanchoice, botchoice); //1,0
+    console.log(results)
+    message = finalmessage(results)  //{message:"you won",color:'green'}
+    console.log(message)
+    // rpsFrontEnd(yourChoice.id, botchoice, message)
+}
+
+function randToRpsInt() {                       //randToRpsInt() ---> Random number to rock paper scissor integer
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number) {
+    return (['rock', 'paper', 'scissors'][number])
+}
+
+function decideWinner(yourChoice, botchoice) {
+    var rpsdatabase = {
+        'rock': { 'scissors': 1, 'rock': 0.5, 'paper': 0 },
+        'paper': { 'scissors': 0, 'rock': 1, 'paper': 0.5 },
+        'scissors': { 'scissors': 0.5, 'rock': 0, 'paper': 1 }
+    }
+    var yourscore = rpsdatabase[yourChoice][botchoice]
+    var computerscore = rpsdatabase[botchoice][botchoice]
+    return [yourscore, computerscore]
+}
+function finalmessage([yourscore, botchoice]) {
+    if (yourscore === 0) {
+        return { 'message': 'you lost', 'color': 'red' };
+
+    } else if (yourscore === 0.5) {
+        return { 'message': 'you tie', 'color': 'yellow' };
+    }
+    else {
+        return { 'message': 'you won', 'color': 'green' };
+    }
+}
